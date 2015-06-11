67a73329fc5a6bba40909136bc53ca603fb6955e
Tapley Stephenson <tapley.stephenson@gmail.com>
2015-06-10 22:08:54 -0700

    add question CRUD routes

--- dbc_overflow/app/controllers/answers_controller.rb
+++ dbc_overflow/app/controllers/answers_controller.rb
@@ -1,2 +1,24 @@
 class AnswersController < ApplicationController
+
+  def new
+    @new_answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def answer_params
+      params.require(:answer).permit(:title, :content, :question_id)
+    end
+
+
 end
--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,11 +1,49 @@
 class QuestionsController < ApplicationController
   def index
+    @new_question = Question.new
     @questions = Question.all.order(created_at: :asc)
+  end
+
+  def new
+    @new_question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   def show
     @question = Question.find(params[:id])
+
+    @new_answer = Answer.new
     @answers = Answer.where(question_id: params[:id])
   end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+    redirect_to question_path
+  end
+
+  def destroy
+    Question.destroy(params[:id])
+    redirect_to questions_path
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def question_params
+      params.require(:question).permit(:title, :content)
+    end
 
 end
